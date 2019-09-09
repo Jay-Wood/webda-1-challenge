011bea277c0e6ca318cc915aa1e6fe276d89af3a
Jay Wood <47437698+jaywood8@users.noreply.github.com>
2019-09-09 11:49:28 -0700

    GET by id working

--- accountsRouter.js
+++ accountsRouter.js
@@ -6,14 +6,28 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
     db("accounts")
-        .select("name","budget")
+        // .select("name","budget")
         .then(accounts => {
             res.status(200).json(accounts)
         })
         .catch(error => {
             res.status(500).json({error: `An error occurred: ${error}`})
         })
-     
 })
+
+router.get("/:id", (req, res) => {
+    let id = req.params.id;
+    
+    db("accounts")
+        .where("id", id)    
+        .then(account => {
+            res.status(200).json(account)
+        })
+        .catch(error => {
+            res.status(500).json({error: `An error occurred: ${error}`})
+        })
+})
+
+
 
 module.exports = router;
